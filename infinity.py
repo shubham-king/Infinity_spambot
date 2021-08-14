9648459906cb5dab11fb403262dd62c9cec4224e
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-08-14 21:42:41 +0530

    Update infinity.py

--- infinity.py
+++ infinity.py
@@ -404,7 +404,7 @@ async def help(e):
 @wdk.on(events.NewMessage(incoming=True, pattern=r"\.alive"))
 async def alive(e):
     if e.sender_id in SMEX_USERS:
-        await event.edit(f"{ALIVE_PIC} 『℡ιɴғιɴιтʏ ꜱρɑɱ ʙ❍т』\n\n➤ Owner: {ALIVE_NAME}\n➤Creater: @Robottech_memeber\n➤Updates: @Philips_bots\n➤ © 「ʟᴇԍᴇɴᴅ ᴘнιʟιᴘs」"
+        await event.edit(f"{ALIVE_PIC} 『℡ιɴғιɴιтʏ ꜱρɑɱ ʙ❍т』\n\n\n Hey bruh I am alive don't worry!\n\n\n➤ Owner: {ALIVE_NAME}\n➤ Creater: @Robottech_memeber\n➤ Updates: @Philips_bots\n © 「ʟᴇԍᴇɴᴅ ᴘнιʟιᴘs」")
         
 
     

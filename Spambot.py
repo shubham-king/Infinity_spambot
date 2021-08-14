6c45a172a995f9ab2b81bc0a9e58520782c2464e
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-08-14 17:46:23 +0530

    </> 『℡ιɴғιɴιтʏ ꜱρɑɱ ʙ❍т』

---
+++ Spambot.py
@@ -0,0 +1,436 @@
+
+import os
+import sys
+import random
+from datetime import datetime
+from os import execl
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateProfileRequest
+from Config import STRING, SUDO, API_ID, API_HASH, STRING2, STRING3, ALIVE_NAME
+import asyncio
+import telethon.utils
+from telethon.tl import functions
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from Utils import RAID, RRAID
+
+
+a = API_ID
+b = API_HASH
+smex = STRING
+smexx = STRING2
+smexxx = STRING3
+
+
+idk = ""
+ydk = ""
+wdk = ""
+
+
+
+que = {}
+
+SMEX_USERS = []
+for x in SUDO:
+    SMEX_USERS.append(x)
+    
+async def start_infinity():
+    global idk
+    global ydk
+    global wdk
+    if smex:
+        session_name = str(smex)
+        print("String 1 Found")
+        idk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 1")
+            await idk.start()
+            botme = await idk.get_me()
+            await idk(functions.channels.JoinChannelRequest(channel="@Philips_bots"))
+            await idk(functions.channels.JoinChannelRequest(channel="@RobotTech_Official"))
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        print("Session 1 not Found")
+        session_name = "startup"
+        idk = TelegramClient(session_name, a, b)
+        try:
+            await idk.start()
+        except Exception as e:
+            pass
+   
+   
+    if smexx:
+        session_name = str(smexx)
+        print("String 2 Found")
+        ydk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 2")
+            await ydk.start()
+            await ydk(functions.channels.JoinChannelRequest(channel="@Philips_bots"))
+            await ydk(functions.channels.JoinChannelRequest(channel="@RobotTech_Official"))
+            botme = await ydk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        print("Session 2 not Found")
+        pass
+        session_name = "startup"
+        ydk = TelegramClient(session_name, a, b)
+        try:
+            await ydk.start()
+        except Exception as e:
+            pass
+
+    if smexxx:
+        session_name = str(smexxx)
+        print("String 3 Found")
+        wdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 3")
+            await  wdk.start()
+            await wdk(functions.channels.JoinChannelRequest(channel="@Philips_bots"))
+            await wdk(functions.channels.JoinChannelRequest(channel="@RobotTech_Official"))
+            botme = await wdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        print("Session 3 not Found")
+        pass
+        session_name = "startup"
+        wdk = TelegramClient(session_name, a, b)
+        try:
+            await wdk.start()
+        except Exception as e:
+            pass
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_yukki())       
+
+async def gifspam(e, smex):
+    try:
+        await e.client(
+            functions.messages.SaveGifRequest(
+                id=types.InputDocument(
+                    id=sandy.media.document.id,
+                    access_hash=smex.media.document.access_hash,
+                    file_reference=smex.media.document.file_reference,
+                ),
+                unsave=True,
+            )
+        )
+    except Exception as e:
+        pass
+
+        
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+join"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-join"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×join"))
+async def _(e):
+    usage = " MODULE NAME = JOIN\n\nCommand:\n\n.join <channel or Group>"
+    if e.sender_id in SMEX_USERS:
+        infinity = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(e.text) > 6:
+            bc = infinity[0]
+            text = "Joining..."
+            event = await e.reply(text, parse_mode=None, link_preview=None )
+            try:
+                await e.client(functions.channels.JoinChannelRequest(channel=bc))
+                await event.edit("Join hogya Sirji")
+            except Exception as e:
+                    await event.edit(str(e))
+            else:
+             await e.reply(usage, parse_mode=None, link_preview=None )
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+leave"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-leave"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×leave"))        
+async def _(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗟𝗲𝗮𝘃𝗲\n\nCommand:\n\n.leave <Channel or Chat ID>"
+    if e.sender_id in SMEX_USERS:
+        infinity = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(e.text) > 7:
+            bc = infinity[0]
+            bc = int(bc)
+            text = "Leaving....."
+            event = await e.reply(text, parse_mode=None, link_preview=None )
+            try:
+                await event.client(LeaveChannelRequest(bc))
+                await event.edit("Succesfully Left")
+            except Exception as e:
+                await event.edit(str(e))   
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+            
+                
+        
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+bigspam"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-bigspam"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×bigspam"))
+async def spam(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗕𝗶𝗴𝗦𝗽𝗮𝗺\n\nCommand:\n\n.bigspam <count> <message to spam>\n\n.bigspam <count> <reply to a message>\n\nCount must be a integer."
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        infinity = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(infinity) == 2:
+            message = str(infinity[1])
+            counter = int(infinity[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "typing"):
+                    if e.reply_to_msg_id:
+                        await smex.reply(message)
+                    else:
+                        await e.client.send_message(e.chat_id, message)
+                    await asyncio.sleep(0.3)
+        elif e.reply_to_msg_id and smex.media:  
+            counter = int(infinity[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "document"):
+                    smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                    await gifspam(e, smex) 
+                await asyncio.sleep(0.3)  
+        elif e.reply_to_msg_id and smex.text:
+            message = smex.text
+            counter = int(infinity[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "typing"):
+                    await e.client.send_message(e.chat_id, message)
+                    await asyncio.sleep(0.3)
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+raid"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-raid"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×raid"))
+async def spam(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗥𝗮𝗶𝗱\n\nCommand:\n\n.raid <count> <Username of User>\n\n.raid <count> <reply to a User>\n\nCount must be a integer."
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        infinity = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(infinity) == 2:
+            message = str(infinity[1])
+            print(message)
+            a = await e.client.get_entity(message)
+            g = a.id
+            c = a.first_name
+            username = f"[{c}](tg://user?id={g})"
+            counter = int(infinity[0])
+            for _ in range(counter):
+                reply = random.choice(RAID)
+                caption = f"{username} {reply}"
+                async with e.client.action(e.chat_id, "typing"):
+                    await e.client.send_message(e.chat_id, caption)
+                    await asyncio.sleep(0.3)
+        elif e.reply_to_msg_id:             
+            a = await e.get_reply_message()
+            b = await e.client.get_entity(a.sender_id)
+            g = b.id
+            c = b.first_name
+            counter = int(infinity[0])
+            username = f"[{c}](tg://user?id={g})"
+            for _ in range(counter):
+                reply = random.choice(RAID)
+                caption = f"{username} {reply}"
+                async with e.client.action(e.chat_id, "typing"):
+                    await e.client.send_message(e.chat_id, caption)
+                    await asyncio.sleep(0.3)
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+
+
+
+
+
+@idk.on(events.NewMessage(incoming=True))
+@ydk.on(events.NewMessage(incoming=True))
+@wdk.on(events.NewMessage(incoming=True))
+async def _(event):
+    global que
+    queue = que.get(event.sender_id)
+    if not queue:
+        return
+    async with event.client.action(event.chat_id, "typing"):
+        await asyncio.sleep(0.3)
+    async with event.client.action(event.chat_id, "typing"):
+        await event.client.send_message(
+            entity=event.chat_id,
+            message="""{}""".format(random.choice(RRAID)),
+            reply_to=event.message.id,
+        )           
+            
+            
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+replyraid"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-replyraid"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×replyraid"))
+async def _(e):
+    global que
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗥𝗲𝗽𝗹𝘆𝗥𝗮𝗶𝗱\n\nCommand:\n\n.replyraid <Username of User>\n\n.replyraid <reply to a User>"
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        infinity = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(e.text) > 11:
+            message = str(infinity[0])
+            a = await e.client.get_entity(message)
+            g = a.id
+            que[g] = []
+            qeue = que.get(g)
+            appendable = [g]
+            qeue.append(appendable)
+            text = "Activated Reply Raid"
+            await e.reply(text, parse_mode=None, link_preview=None )
+        elif e.reply_to_msg_id:             
+            a = await e.get_reply_message()
+            b = await e.client.get_entity(a.sender_id)
+            g = b.id
+            que[g] = []
+            qeue = que.get(g)
+            appendable = [g]
+            qeue.append(appendable)
+            text = "Activated Reply Raid"
+            await e.reply(text, parse_mode=None, link_preview=None )
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+
+            
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+dreplyraid"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-dreplyraid"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×dreplyraid"))
+async def _(e):
+    global que
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗗𝗲𝗮𝗰𝘁𝗶𝘃𝗮𝘁𝗲 𝗥𝗲𝗽𝗹𝘆𝗥𝗮𝗶𝗱\n\nCommand:\n\n.dreplyraid <Username of User>\n\n.dreplyraid <reply to a User>"
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        infinity = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(e.text) > 12:
+            message = str(infinity[0])
+            a = await e.client.get_entity(message)
+            g = a.id
+            try:
+                queue = que.get(g)
+                queue.pop(0)
+            except Exception as f:
+                pass
+            text = "De-Activated Reply Raid"
+            await e.reply(text, parse_mode=None, link_preview=None )
+        elif e.reply_to_msg_id:             
+            a = await e.get_reply_message()
+            b = await e.client.get_entity(a.sender_id)
+            g = b.id
+            try:
+                queue = que.get(g)
+                queue.pop(0)
+            except Exception as f:
+                pass
+            text = "De-Activated Reply Raid"
+            await e.reply(text, parse_mode=None, link_preview=None )
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+    
+       
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+ping"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-ping"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×ping"))
+async def ping(e):
+    if e.sender_id in SMEX_USERS:
+        start = datetime.now()
+        text = "Pong!"
+        event = await e.reply(text, parse_mode=None, link_preview=None )
+        end = datetime.now()
+        ms = (end-start).microseconds / 1000
+        await event.edit(f"🤖 𝗣𝗼𝗻𝗴!\n`{ms}` 𝗺𝘀 {ALIVE_NAME}\n Powered by @Philips_bots ")
+
+
+
+        
+        
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+restart"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-restart"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×restart"))
+async def restart(e):
+    if e.sender_id in SMEX_USERS:
+        text = "𝙍𝙚𝙨𝙩𝙖𝙧𝙩𝙚𝙙\n\nPlease wait till it reboots..."
+        await e.reply(text, parse_mode=None, link_preview=None )
+        try:
+            await idk.disconnect()
+        except Exception as e:
+            pass
+        try:
+            await ydk.disconnect()
+        except Exception as e:
+            pass
+        try:
+            await wdk.disconnect()
+        except Exception as e:
+            pass
+        os.execl(sys.executable, sys.executable, *sys.argv)
+        quit()
+
+        
+@idk.on(events.NewMessage(incoming=True, pattern=r"\+help"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\-help"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\×help"))
+async def help(e):
+    if e.sender_id in SMEX_USERS:
+        text = "𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 𝐂𝐎𝐌𝐌𝐌𝐀𝐍𝐃𝐒 𝐈𝐍 『℡ιɴғιɴιтʏ ꜱρɑɱ ʙ❍т』 \n\n𝐔𝐓𝐈𝐋𝐒 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.ping\n.restart\n\n𝐔𝐒𝐄𝐑𝐁𝐎𝐓 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.bio\n.join\n.leave\n.pleave\n\n𝐒𝐏𝐀𝐌 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.spam\n.delayspam\n.bigspam\n.raid\n.replyraid\n.dreplyraid\n\n\nFor more help regarding usage of plugins type plugins name\n Powered by @Philips_bots"
+        
+        await e.reply(text, parse_mode=None, link_preview=None )
+       
+        
+
+    
+        
+text = """ © Philips bot PVT. LTD. """
+print(text)
+print("")
+print("Philis  Multi Spam Bot Started Sucessfully.")
+if len(sys.argv) not in (1, 3, 4):
+    try:
+        idk.disconnect()
+    except Exception as e:
+        pass
+    try:
+        ydk.disconnect()
+    except Exception as e:
+        pass
+    try:
+        wdk.disconnect()
+    except Exception as e:
+        pass
+else:
+    try:
+        idk.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        ydk.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        wdk.run_until_disconnected()
+    except Exception as e:
+        pass 
+
+# ERROR FIXED BY © Alone_loverboy 

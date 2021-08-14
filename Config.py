d9ef98f12426b59c91a75609c21dfaac8adb7e45
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-08-14 15:23:42 +0530

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,12 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+STRING = getenv("STRING")
+STRING2 = getenv("STRING2")
+STRING3 = getenv("STRING3")
+API_ID = getenv("API_ID")
+API_HASH = getenv("API_HASH")
+ALIVE_NAME = getenv("ALIVE_NAME")
+SUDO = list(map(int, getenv("SUDO").split()))
